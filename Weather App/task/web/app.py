0df06aea0a22d9b419ed7b01c3a227b04b0f83e3
barrykollet1 <barrykollet1@yahoo.fr>
2021-05-04 20:54:01 +0000

    Hello world!

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -3,6 +3,12 @@ import sys
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return "Hello, world!"
+
+
 # don't change the following way to run flask:
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -10,3 +16,6 @@ if __name__ == '__main__':
         app.run(host=arg_host, port=arg_port)
     else:
         app.run()
+
+
+
